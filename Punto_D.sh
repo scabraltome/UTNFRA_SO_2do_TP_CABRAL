f92d22d16ceb8c9c3ba032b37e0c3a3e21890d1e
scabraltome <scabraltome@gmail.com>
2026-06-17 20:36:24 +0000

    Punto D - Script SSH corregido con permisos de seguridad

--- Punto_D.sh
+++ Punto_D.sh
@@ -2,4 +2,5 @@ ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
-ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@192.168.56.12
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
